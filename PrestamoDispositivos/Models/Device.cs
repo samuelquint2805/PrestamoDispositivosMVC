4a0c510c55e17a20fcac54e9c4b6cb408b7f92c7
samuel Quintero <quinterosamuel0528@gmail.com>
2026-04-04 15:34:04 -0500

    CREACION DE PROYECTO DSI

--- PrestamoDispositivos/Models/Device.cs
+++ PrestamoDispositivos/Models/Device.cs
@@ -12,18 +12,18 @@ namespace PrestamoDispositivos.Models
         [Key]
         [Required]
         public Guid IdDisp { get; set; }
-        [Required(ErrorMessage = "El campo de Tipo es requerido")]
-        public string? Tipo { get; set; }
+       
         [Required(ErrorMessage = "El campo de Serial es requerido")]
         public string? Serial { get; set; }
-        [Required(ErrorMessage = "El campo de Procesador es requerido")]
-        public string? Procesador { get; set; }
-        [Required(ErrorMessage = "El campo de Almacenamiento es requerido")]
-        public int? Almacenamiento { get; set; }
-        [Required(ErrorMessage = "El campo de TarjetaGráfica es requerido")]
-        public  string? TarjetaGrafica { get; set; }
+        [Required(ErrorMessage = "El campo de Marca es requerido")]
+        public string? Marca { get; set; }
+        [Required(ErrorMessage = "El campo de Especificaciones es requerido")]
+        public string? Especificaciones { get; set; }
         [Required(ErrorMessage = "El campo de Estado del Dispositivo es requerido")]
-        public  string? EstadoDisp { get; set; }
+        public  string? EstadoEquipo { get; set; }
+        [Required(ErrorMessage = "El campo de Imagen del Dispositivo es requerido")]
+        [MaxLength(500)]
+        public string? URLImagen { get; set; }
         #endregion
 
         // apartado para Relaciones con otras clases (tablas)
